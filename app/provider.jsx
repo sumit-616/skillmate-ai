53e6a0446ed5d6221e0374f26979748e68c17b36
sumit-616 <sumit616794@gmail.com>
2025-04-18 18:45:58 +0530

    Update provider.jsx

--- app/provider.jsx
+++ app/provider.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const Provider = ({children}) => {
+const Provider = ({ children }) => {
   const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
     <div>
