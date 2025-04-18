1fc5736e5910f5fa66057419416d06005b880074
sumit-616 <sumit616794@gmail.com>
2025-04-19 00:36:25 +0530

    Update AuthProvider.jsx

--- app/AuthProvider.jsx
+++ app/AuthProvider.jsx
@@ -1,18 +1,29 @@
+import { api } from '@/convex/_generated/api';
 import { useUser } from '@stackframe/stack'
+import { useMutation } from 'convex/react';
 import React, { useEffect } from 'react'
 
 const AuthProvider = ({children}) => {
   const user = useUser();
+  const CreateUser = useMutation(api.users.CreateUser);
 
   useEffect(()=>{
     console.log(user);
   },[user]);
 
+
+  const CreateNewUser = async ()=>{
+    const result = await CreateUser({
+      name:user?.displayName,
+      email: user?.primaryEmail,
+    })
+  }
+
   return (
     <div>
       {children}
     </div>
-  )
+  ) 
 }
 
 export default AuthProvider;
