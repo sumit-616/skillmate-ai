5ccd6aaf96190890eed68b67839f37227cfc093e
sumit-616 <sumit616794@gmail.com>
2025-04-20 07:18:25 +0530

    Update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import AppHeader from './_components/AppHeader';
 
 const DashboardLayout = ({children}) => {
   return (
     <div>
-      {children}
+      <AppHeader />
+      <div className='p-10 mt-20 md:px-20 lg:px-32 xl:px-56 2xl:px-72'>
+        {children}
+      </div>
     </div>
   )
 }
