eb2a28ce5b612032ff34c122a757d2f8567b8cce
sumit-616 <sumit616794@gmail.com>
2025-04-21 01:27:05 +0530

    Update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({children}) => {
   return (
     <div>
       <AppHeader />
-      <div className='p-10 mt-20 md:px-20 lg:px-32 xl:px-56 2xl:px-72'>
+      <div className='p-10 mt-10 md:px-15 lg:px-24 xl:px-44 2xl:px-64'>
         {children}
       </div>
     </div>
