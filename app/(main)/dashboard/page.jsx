82a263afb62163716deece96da31f288b894d94b
sumit-616 <sumit616794@gmail.com>
2025-07-26 03:14:57 +0530

    Update page.jsx

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,12 +1,12 @@
+import FeatureAssistant from '@components/custom/FeatureAssistant'
+import Feedback from '@components/custom/Feedback'
+import History from '@components/custom/History'
 import React from 'react'
-import FeatureAssitants from './_components/FeatureAssistants';
-import History from './_components/History';
-import Feedback from './_components/Feedback';
 
 const Dashboard = () => {
   return (
     <div>
-      <FeatureAssitants/>
+      <FeatureAssistant/>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-20'>
         <History/>
         <Feedback/>
