7b00e00ded1e4795f387df77c8a27481757f11c9
sumit-616 <sumit616794@gmail.com>
2025-07-26 03:18:35 +0530

    Updated as per new configuration.

--- middleware.jsx
+++ middleware.jsx
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { stackServerApp } from "./stack";
+import { stackServerApp } from "@/stack";
 
 export async function middleware(request) {
   const user = await stackServerApp.getUser();
@@ -12,5 +12,5 @@ export async function middleware(request) {
 export const config = {
   // You can add your own route protection logic here
   // Make sure not to protect the root URL, as it would prevent users from accessing static Next.js files or Stack's /handler path
-  matcher: '/protected/:path*',
+  matcher: '/dashboard/:path*',
 };
