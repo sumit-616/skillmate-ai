5a6e065615c6f55c20c3b260f68468125affd757
sumit-616 <sumit616794@gmail.com>
2025-07-26 03:17:37 +0530

    Moved components from app/ to a generic components/ folder for better structure and maintainability.

---
+++ components/custom/FeatureAssistant.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React from 'react'
+import Image from 'next/image';
+import { useUser } from '@stackframe/stack';
+import { CoachingExpert, CoachingOptions } from '@services/Options';
+import { Button } from '@components/ui/button';
+import { BlurFade } from '@components/magicui/blur-fade';
+
+const FeatureAssistant = () => {
+  const user = useUser();
+
+  return (
+    <div>
+      <div className='flex justify-between items-center'>
+        <div>
+          <h2 className='font-medium text-gray-500'>My Workspace</h2>
+          <h2 className='text-3xl font-bold'>Welcome back, {user?.displayName}</h2>
+        </div>
+        <Button className='cursor-pointer'>Profile</Button>
+      </div>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-10 mt-10'>
+        {CoachingOptions.map((option, index) => (
+          <BlurFade key={option.icon} delay={0.25 + index * 0.10} inView>
+            <div className='p-3 bg-secondary rounded-3xl flex flex-col justify-center items-center cursor-pointer'>
+              <Image src={option.icon} alt={option.name} width={150} height={150} className='h-[70px] w-[70px] hover:rotate-12 hover:scale-3d hover:scale-105 transition-all' />
+              <h2 className='mt-2'>{option.name}</h2>
+            </div>
+          </BlurFade>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default FeatureAssistant
---
+++ components/custom/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const History = () => {
+  return (
+    <div>
+      <h2 className='font-bold text-xl'>Your Previous Lectures</h2>
+      <h2 className='text-gray-400'>You don't have any previous lecture</h2>
+    </div>
+  )
+}
+
+export default History
