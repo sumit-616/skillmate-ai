78aa79d1148c4d09977469ec0d2e9525e5d91ad0
sumit-616 <sumit616794@gmail.com>
2025-07-26 03:18:18 +0530

    Updated as per new configuration.

--- convex/users.js
+++ convex/users.js
@@ -1,23 +1,23 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation } from "@/convex/_generated/server"
 
 export const CreateUser = mutation({
-  args:{
+  args: {
     name: v.string(),
     email: v.string(),
   },
-  handler: async(ctx, args)=>{
+  handler: async (ctx, args) => {
     // if user already exists
-    const userData = await ctx.db.query('users').filter(q => q.eq(q.field('email'),args.email)).collect();
-    
+    const userData = await ctx.db.query('users').filter(q => q.eq(q.field('email'), args.email)).collect();
+
     // if not then add new user
-    if(userData?.length==0){
+    if (userData?.length == 0) {
       const data = {
         name: args.name,
         email: args.email,
         credits: 50000,
       }
-      const result = await ctx.db.insert('users',{
+      const result = await ctx.db.insert('users', {
         ...data
       });
       // console.log(result);
