b128aa1d8cff84dbcb1a22d6f7dfe0abfec53f2f
sumit-616 <sumit616794@gmail.com>
2025-07-27 16:47:29 +0530

    changed the padding value in layout for better ui.

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -5,11 +5,12 @@ const DashboardLayout = ({ children }) => {
   return (
     <div>
       <AppHeader />
-      <div className='p-10 mt-14 md:px-20 lg:px-32 xl:px-56 2xl:px-72'>
+      <div className='p-10 mt-20 md:px-12 lg:px-20 xl:px-36 2xl:px-52'>
         {children}
       </div>
     </div>
   )
 }
+
 
 export default DashboardLayout;
