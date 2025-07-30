89adab43f64b136ec0ee98875bb61842f9b5fb60
sumit-616 <sumit616794@gmail.com>
2025-07-31 01:23:27 +0530

    Create DiscussionRoom.jsx

---
+++ convex/DiscussionRoom.jsx
@@ -0,0 +1,69 @@
+import { mutation, query } from "@/convex/_generated/server";
+import { v } from "convex/values";
+
+export const CreateNewRoom = mutation({
+  args: {
+    coachingOption: v.string(),
+    topic: v.string(),
+    expertName: v.string(),
+    uid: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("DiscussionRoom", {
+      coachingOption: args.coachingOption,
+      topic: args.topic,
+      expertName: args.expertName,
+      uid: args.uid,
+    });
+
+    return result;
+  },
+});
+
+export const GetDiscussionRoom = query({
+  args: {
+    id: v.id("DiscussionRoom"),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db.get(args.id);
+    return result;
+  },
+});
+
+export const UpdateConversation = mutation({
+  args: {
+    id: v.id("DiscussionRoom"),
+    conversation: v.any(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.id, {
+      conversation: args.conversation,
+    });
+  },
+});
+
+export const UpdateSummary = mutation({
+  args: {
+    id: v.id("DiscussionRoom"),
+    summary: v.any(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.patch(args.id, {
+      summary: args.summary,
+    });
+  },
+});
+
+export const GetAllDiscussionRoom = query({
+  args: {
+    uid: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db
+      .query("DiscussionRoom")
+      .filter((q) => q.eq(q.field("uid"), args.uid))
+      .order('desc')
+      .collect();
+    return result;
+  },
+});
